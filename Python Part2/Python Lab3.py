af53200a50ee0c581f66ca8e92fc011477bf42aa
veggies <ryanruddock96@gmail.com>
2024-06-25 17:10:41 -0600

    The last two labs got a little tricky!

--- Python Part2/Python Lab3.py
+++ Python Part2/Python Lab3.py
@@ -24,3 +24,70 @@
 #    print("You must be one of Donald's nephews!")
 #else:
 #    print("I'm not sure whose nephew you are...")
+
+#The table below outlines the grade boundaries on a certain university course. Please write
+#a program which asks for the amount of points received and then prints out the grade attained
+#according to the table.
+#points = int(input("How many points did you score?"))
+#if points < 0:
+#    print("Impossible!")
+#elif points >= 0 and points <= 49:
+#    print("Fail!")
+#elif points >= 50 and points <= 59:
+#    print("Grade: 1")
+#elif points >= 60 and points <= 69:
+#    print("Grade: 2")
+#elif points >= 70 and points <= 79:
+#    print("Grade: 3")
+#elif points >= 80 and points <= 89:
+#    print("Grade: 4")
+#elif points >= 90 and points <= 100:
+#    print("Grade: 5")
+#else:
+#    print("Impossible!")
+
+#Please write a program which asks the user for an integer number. If the number is divisible by
+#three, the program should print out Fizz. If the number is divisible by five, the program should
+#print out Buzz. If the number is divisible by both three and five, the program should print out
+#FizzBuz
+#number = int(input("Please provide a number: "))
+#if number % 5 == 0 and number % 3 ==0:
+#    print("FizzBuzz")
+#elif number % 5 == 0:
+#    print("Buzz")
+#elif number % 3 == 0:
+#    print("Fizz")
+
+#Please write a program which asks the user for a year, and then prints out whether that year is
+#a leap year or not.
+#year = int(input("Please type in a year:"))
+#if year > 0:
+#    if year % 100 == 0 and year % 4 ==0 and year % 400 == 0:
+#        print("That year is a leap year!")
+#    elif year % 4 == 0 and not year % 100 ==0:
+#        print("That year is a leap year!")
+#    else:
+#        print("That year is not a leap year")
+
+#Please write a program which asks the user for three letters. The program should then print out
+#whichever of the three letters would be in the middle if the letters were in alphabetical order.
+#You may assume the letters will be either all uppercase or all lowercase.
+#Some example of expect behaviour.
+#letter1 = input("Please provide one letter: ")
+#letter2 = input("Please provide one letter: ")
+#letter3 = input("Please provide one letter: ")
+#if letter1 > letter2 and letter1 > letter3:
+#    if letter2 > letter3:
+#        print(f"The letter in the middle is {letter2}")
+#    elif letter3 > letter2:
+#        print(f"The letter in the middle is {letter3}")
+#elif letter2 > letter1 and letter2 > letter3:
+#    if letter3 > letter1:
+#        print(f"The letter in the middle is {letter3}")
+#    elif letter1 > letter3:
+#        print(f"The letter in the middle is {letter1}")
+#elif letter3 > letter1 and letter3 > letter2:
+#    if letter2 > letter1:
+#        print(f"The letter in the middle is {letter2}")
+#    elif letter1 > letter2:
+#        print(f"The letter in the middle is {letter1}")
