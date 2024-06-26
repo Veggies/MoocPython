73ea27e14c0320a2f2063c95469b10a7e5f30724
veggies <ryanruddock96@gmail.com>
2024-06-26 16:33:12 -0600

    Working with while true statements in lesson 4 of part2!

---
+++ Python Part2/Python Lab4.py
@@ -0,0 +1,15 @@
+#This lesson first introduces the while loop. The while loop will continue to do whatever it's
+#coded to do until the action associate with a break is performed. The while loop, will, loop.
+#You can use a break to allow voluntary exit, such as typing -1, or as a way to perform an 
+#authentication check of sorts, such as requiring a certain unspecified phrase or number be
+#entered.
+ 
+#This program should print out the message "hi" and then ask "Shall we continue?" until the user
+#inputs "no". Then the program should print out "okay then" and finish.
+
+#while True:
+#    print("hi")
+#    hello = input(print("Should we continue?"))
+#    if hello == 'no':
+#        break
+#print("okay then")
