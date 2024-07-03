59ce2097f29a4b8a280aaaf6cee80be54a8ec450
veggies <ryanruddock96@gmail.com>
2024-07-03 17:32:17 -0600

    Start lab2 part3

---
+++ Python Part3/Python Lab2.py
@@ -0,0 +1,31 @@
+#string = input("String: ")
+#repeat = int(input("Repeat: "))
+#print((string) * (repeat))
+
+#string1 = input("String 1: ")
+#string2 = input("String2: ")
+#if len(string1) > len(string2):
+#    print(f"{string1} is longer")
+#else:
+#    print(f"{string2} is longer")
+
+#string = input("String: ")
+#length = len(string) - 1
+##print(length)
+#while length > 0:
+#    print(string[length])
+#    length -= 1
+#if len(string) > 0:
+#    print(string[0])
+
+#string = input("String: ")
+#while len(string) >= 3:
+#    if string[1] == string[-2]:
+#        print(f"The second and second to last characters are: {string[1]}")
+#        break
+#    else:
+#        print("The second and second to last characters are different.")
+#        break
+#if len(string) < 3:
+#    print("String must be greater than 3.")
+
