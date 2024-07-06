9026e0eec30d56af227be562fd254390fb38d65e
Veggies <ryanruddock96@gmail.com>
2024-07-05 22:30:10 -0700

    Completed some more labs. Happy 5th of July.

--- Python Part3/Python Lab2.py
+++ Python Part3/Python Lab2.py
@@ -29,3 +29,35 @@
 #if len(string) < 3:
 #    print("String must be greater than 3.")
 
+#length = int(input("Length of hashes: "))
+#height = int(input("Height of hashes: "))
+#print(f"#"*length)
+#height2 = 0
+#while height2 < height:
+##    print(height2) This line was used to figure out that the program prints out a line whilst height2 is 0
+##                   which let me figure out the height - 1 if condition.
+#    if height2 == height - 1:
+#        break
+#    print(f"#"*length)
+#    height2 += 1
+
+#while True:
+#    string = input("Please type in a string: ")
+#    print(string)
+#    print(f"-"*len(string))
+#    if string == "":
+#        break
+
+#string=input("Please type in a string: ")
+#if len(string) < 20:
+#    print(f"*"*(20 - len(string))+string)
+
+#string=input("Please type in a string: ")
+#print("*"*30)
+#middle=28
+#middle = middle - len(string)
+#middle = middle // 2
+##print(middle) Was using this line to test variable override. Initially stuck middle at the end with a ==
+##but realized you have to redefine the variable at the start for override to occur.
+#print("*"+" "*middle+string+" "*middle+"*")
+#print("*"*30)
