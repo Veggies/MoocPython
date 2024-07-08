3ca9a9bdf39b9aa872fbece69003d35a6fab8550
veggies <ryanruddock96@gmail.com>
2024-07-08 16:02:26 -0600

    These labs are gettin hard!!!

---
+++ Python Part3/Python Lab3.py
@@ -0,0 +1,31 @@
+#number=int(input("Please provide a positive integer number: "))
+#while number %2 != 0:
+#    number=int(input("Number must be positive: "))
+#operand1 = 1
+#operand2 = 1
+#while operand1 and operand2 != number:
+#    while operand1 <= number:
+#        print(f"{operand1} x {operand2} = {operand1*operand2}")
+#        if operand1 == number:
+#            break
+#        operand1 += 1
+#    while operand2 <= number:
+#        print(f"{operand1} x {operand2} = {operand1*operand2}")
+#        if operand2 == number:
+#            break
+#        operand2 += 1
+
+#sentence=input("Please type in a fun creative sentence: ")
+#print(sentence[0])
+#space1=sentence.find(" ")
+#index=0
+#while True:
+##    if index >= len(sentence):
+#        break
+#    while index>=len(sentence) or sentence[index]!=" ":
+#        index+=1
+#        if index>=len(sentence):
+#            break
+#        if sentence[index]==" ":
+#            print(sentence[index+1])
+#    index+=1
