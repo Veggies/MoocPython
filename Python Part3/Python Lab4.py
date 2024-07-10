15e309a6e915934c93a2ccb4c2fab7a3309e0831
veggies <ryanruddock96@gmail.com>
2024-07-10 15:19:08 -0600

    Starting to not complete ALL labs. Not necessary.

---
+++ Python Part3/Python Lab4.py
@@ -0,0 +1,28 @@
+#def seven_brothers():
+#    print("Aapo")
+#    print("Eero")
+#seven_brothers()
+
+#def first_character(text):
+#    print(text[0])
+#first_character(text=input("Type in a string: "))
+
+#def mean(x, y, z):
+#    result=(x * y * z)/3
+#    print(result)
+#mean(5, 12, 8)
+
+#def print_many_times(text, times):
+#    repeat=0
+#    while repeat < times:
+#        print(text)
+#        repeat+=1
+#print_many_times("python",3)
+
+#def hash_square(length):
+#    hashes=0
+#    while hashes < length:
+#        print("#"*length)
+#        hashes+=1
+#hash_square(7)
+
