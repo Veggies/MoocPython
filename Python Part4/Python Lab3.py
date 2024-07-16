cf9d3a053e1953c52f3c3a39a01f7ff0e7da2f5b
veggies <ryanruddock96@gmail.com>
2024-07-15 19:55:09 -0600

    Shift change make me too sleepy :(

---
+++ Python Part4/Python Lab3.py
@@ -0,0 +1,40 @@
+#list = [1, 2, 3, 4, 5]
+#index=1
+#index=int(input("Index: "))
+#list[index]=int(input("New Value: "))
+#print(list)
+
+#list=[]
+#numberadd=int(input("How many items? "))
+#totaladd=1
+#nn=1
+#while totaladd <= numberadd:
+#    nn=int(input(f"Item {totaladd}: "))
+#    list.append(nn)
+#    totaladd+=1
+#print(list)
+
+#list=[]
+#add=1
+#while True:
+#    ard=input("Ad(d), (r)emove, or e(x)it: ")
+#    if ard == "x":
+#        break
+#    if ard=="d":
+#        list.append(add)
+#        add+=1
+#    if ard=="r":
+#        if add!=1:
+#            list.pop(add-2)
+#            add-=1
+#    print(list)
+
+#list=[]
+#total=0
+#while True:
+#    word=input("Word:")
+#    if word in list:
+#        break
+#    list.append(word)
+#    total+=1
+#print(f"You typed in {total} words.")
